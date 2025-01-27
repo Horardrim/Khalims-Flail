f74432dd1081bd063f566d653d5e04deb93b58e2
TalRasha <524948250@qq.com>
2025-01-27 10:39:39 +0800

    first push

--- src/main/java/com/khalims/flail/spark/tasks/SampleSparkTask.java
+++ src/main/java/com/khalims/flail/spark/tasks/SampleSparkTask.java
@@ -1,5 +1,35 @@
 package com.khalims.flail.spark.tasks;
 
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
 public class SampleSparkTask {
-    
+    public static void main(String[] args) {
+        // 创建Spark配置
+        SparkConf conf = new SparkConf().setAppName("SampleSparkTask").setMaster("local[*]");
+        
+        // 创建Spark上下文
+        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
+            
+            // 加载数据
+            JavaRDD<String> lines = sc.textFile("resources/WordCount.txt");
+
+            // 分割单词并映射为(key, value)形式
+            JavaPairRDD<String, Integer> wordCounts =
+                lines.flatMap(s -> Arrays.asList(s.split("\\s+")).iterator())
+                    .mapToPair(word -> new Tuple2<>(word.toLowerCase(), 1))
+                    .reduceByKey((a, b) -> a + b);
+
+            // 收集结果并打印
+            for (Tuple2<?, ?> tuple : wordCounts.collect()) {
+                System.out.println(tuple._1() + ": " + tuple._2());
+            }
+        }
+    }
 }
